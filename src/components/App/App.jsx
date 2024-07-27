861cb91f1c5ce6a7075fdc293a58473bbfb1fbb8
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-07-27 11:38:05 +0300

    fix app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,35 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
-import Navigation from '../../components/Navigation';
+import Navigation from '../Navigation/Navigation';
 
-const HomePage = lazy(() => import('../../pages/HomePage'));
-const HelpDonationPage = lazy(() => import('../../pages/HelpDonationPage'));
-const NewsPage = lazy(() => import('../../pages/NewsPage'));
-const SearchPage = lazy(() => import('../../pages/SearchPage'));
-const AboutUsPage = lazy(() => import('../../pages/AboutUsPage'));
-const MyAccountPage = lazy(() => import('../../pages/MyAccountPage'));
-const SignInPage = lazy(() => import('../../pages/SignInPage'));
-const SignUpPage = lazy(() => import('../../pages/SignUpPage'));
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const HelpDonationPage = lazy(() => import('../../pages/HelpDonationPage/HelpDonationPage'));
+const NewsPage = lazy(() => import('../../pages/NewsPage/NewsPage'));
+const SearchPage = lazy(() => import('../../pages/SearchPage/SearchPage'));
+const AboutUsPage = lazy(() => import('../../pages/AboutUsPage/AboutUsPage'));
+const MyAccountPage = lazy(() => import('../../pages/MyAccountPage/MyAccountPage'));
+const SignInPage = lazy(() => import('../../pages/SignInPage/SignInPage'));
+const CreateAccountPage = lazy(() => import('../../pages/CreateAccountPage/CreateAccountPage'));
+const CategoriesPage = lazy(() => import('../../pages/CategoriesPage/CategoriesPage'))
 
 const App = () => {
   return (
-    <>
+    <div>
       <Navigation />
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
+          <Route path='/categories' element={<CategoriesPage/>}/>
           <Route path="/news" element={<NewsPage />} />
           <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/help-donations" element={<HelpDonationPage />} />
+          <Route path="/donations" element={<HelpDonationPage />} />
           <Route path="/my-account" element={<MyAccountPage />} />
           <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/registration" element={<CreateAccountPage />} />
         </Routes>
       </Suspense>
-    </>
+    </div>
   );
 };
 
